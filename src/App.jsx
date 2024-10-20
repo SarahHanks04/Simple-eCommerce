53e183b0728c90e1ca9c8bd89018e1dd1c44186a
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-20 21:56:42 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
+import Collection from "./Pages/Collection"
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
       </Routes>
     </div>
   )
