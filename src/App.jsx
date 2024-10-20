a6ee12c5c827ff667e1629efb9a43467272d57dd
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-20 21:41:09 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
 
 function App() {
   
 
   return (
-    <>
-      <div></div>
-    </>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
