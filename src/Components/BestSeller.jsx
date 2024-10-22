8a76142138e1acf97e1ac925d8df2ee2f58babfb
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-22 21:14:14 +0100

    updated

--- src/Components/BestSeller.jsx
+++ src/Components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../Context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
@@ -19,6 +20,13 @@ const BestSeller = () => {
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum happens then.
         </p>
+      </div>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+            bestSeller.map((item, index) => (
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+            ))
+        }
       </div>
     </div>
   )
