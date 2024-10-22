2b9349d29aeccf0984413aacb17491b0560ee47b
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-22 19:29:58 +0100

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="" />
+      <Link to='/'><img src={assets.logo} className="w-36" alt="" /></Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -63,6 +63,7 @@ const Navbar = () => {
           alt=""
         />
       </div>
+
       {/* Sidebar menu for small screens */}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
