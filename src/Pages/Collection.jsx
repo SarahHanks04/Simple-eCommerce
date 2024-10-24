c55e64e0d8f286b325250bdcea9c718a274d8462
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-24 16:08:12 +0100

    updated

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -15,9 +15,9 @@ const Collection = () => {
       
       {/* FILTER OPTIONS */}
       <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS 
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS 
         <img className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""} sm:block`} src={assets.dropdown_icon} alt="" /></p>
-        
+
         {/* CATEGORY FILTER */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? " " : "hidden"} sm:block`}>
             <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
