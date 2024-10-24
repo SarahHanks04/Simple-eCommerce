aa2a3fc95f25cb30cd2206f2572f5121a95fc9bc
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-24 14:49:41 +0100

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,24 @@ const Footer = () => {
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-            <img src={assets.logo} className='mb' alt="" />
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the start when on unknown inter hook to the state of scrambled place to make a type slip of book.</p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>Company</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Delivery</li>
+              <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>contact@</li>
+          </ul>
         </div>
       </div>
     </div>
