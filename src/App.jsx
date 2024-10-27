8de0feaea95f2d2f55a2d07ab463aa50ba7e7785
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-27 19:55:16 +0100

    React Toastify installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from "react"
-import './App.css'
-import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-import Collection from "./Pages/Collection"
-import About from "./Pages/About"
-import Contact from "./Pages/Contact"
-import Product from "./Pages/Product"
-import Cart from "./Pages/Cart"
-import Login from "./Pages/Login"
-import PlaceOrder from "./Pages/PlaceOrder"
-import Orders from "./Pages/Orders"
-import Navbar from "./Components/Navbar"
-import Footer from "./Components/Footer"
-import SearchBar from "./Components/SearchBar"
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Orders from "./Pages/Orders";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import SearchBar from "./Components/SearchBar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  
-
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-    <Navbar />
-    <SearchBar />
+    <ToastContainer />
+      <Navbar />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
@@ -34,7 +35,7 @@ function App() {
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
