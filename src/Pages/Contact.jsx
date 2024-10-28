1629f99182f7b99528235b74368330aabc6fa320
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-28 02:27:12 +0100

    About updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Title from '../Components/Title'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
     </div>
   )
 }
