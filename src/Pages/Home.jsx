e1e8b983fa07560adbea8ca39d89ab0786e8ae8d
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-30 22:49:42 +0100

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Hero from '../Components/Hero';
-import LatestCollection from '../Components/LatestCollection';
-import BestSeller from '../Components/BestSeller';
-import OurPolicy from '../Components/OurPolicy';
-import NewsLetterBox from '../Components/NewsLetterBox';
+import React from "react";
+import Hero from "../Components/Hero";
+import LatestCollection from "../Components/LatestCollection";
+import BestSeller from "../Components/BestSeller";
+import OurPolicy from "../Components/OurPolicy";
+import NewsLetterBox from "../Components/NewsLetterBox";
 
 const Home = () => {
   return (
@@ -14,7 +14,7 @@ const Home = () => {
       <OurPolicy />
       <NewsLetterBox />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
