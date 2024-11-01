e75782ea8e2030ab49170f875f58482684a921f0
SarahHanks04 <sarahhanks04@gmail.com>
2024-11-01 10:40:13 +0100

    Formating

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-
         <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
           <p className="w-full md:w-2/3 text-gray-600">
@@ -37,7 +36,9 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">Copyright 2024@ forever.com - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ forever.com - All Right Reserved.
+        </p>
       </div>
     </div>
   );
